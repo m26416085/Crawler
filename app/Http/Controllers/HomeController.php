054a08d3bc301aee5548a3ab967b03a75accd5ba
m26416085 <37286160+m26416085@users.noreply.github.com>
2019-07-19 10:59:49 +0700

    Cart Tokopedia

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,9 +1,10 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use View;
+use Darryldecode\Cart\Cart;
+
 class HomeController extends Controller
 {
     /**
@@ -11,6 +12,9 @@ class HomeController extends Controller
      *
      * @return void
      */
+
+    public $item_count = 0;
+
     public function __construct()
     {
         $this->middleware('auth');
@@ -23,40 +27,87 @@ class HomeController extends Controller
      */
     public function index()
     {
+        //default cart item
+        \Cart::add(array(
+            'id' => 9999,
+            'name' => "asdfghjklkjgfds123890ythbnvdkodetokopediafwgheu3yr2t3r64ortfg",
+            'price' => 100000,
+            'quantity' => 1,
+            'attributes' => array(
+                'image_url' => "////",
+                'price_format' => "Rp. 100000",
+                'shop_name' => "ertertrtrgv",
+                'shop_location' => "ghrtyrghjnvfg",
+                'product_url' => "////"
+            )
+        ));
+
+        $cartCollection = \Cart::getContent();
+
+        $cartCollection->toArray();
+
+        //list all available city in tokopedia
+        $city_url = "https://ace.tokopedia.com/v4/dynamic_attributes?scheme=https&device=desktop&related=true&page=9&ob=23&st=product&fcity=0&source=search&q=&unique_id=c8c1be17273f45f9b5d1c5346b116570&safe_search=false";
+        $data_city = http_request($city_url);
+        $data_city = json_decode($data_city, TRUE);
+
         $data_arr = array();
             
         //tokopedia
     
-        $url = "https://ta.tokopedia.com/promo/v1/display/ads?user_id=0&ep=product&item=10&src=search&device=desktop&page=2&q=mouse&fshop=1";
-    
+        $url = "https://ace.tokopedia.com/search/product/v3?scheme=https&device=desktop&related=true&catalog_rows=5&source=search&ob=23&st=product&rows=200&start=0&q=mouse&unique_id=a5e21c08aa434ccda179065dc7e41c73&safe_search=false";
+
         $profile = http_request($url);
         $profile = json_decode($profile, TRUE);
     
         $i = 0;
         foreach ($profile["data"] as $profil) {
-            $image_url = $profile['data'][$i]['product']['image']['m_url'];
-            $product_name = $profile['data'][$i]['product']['name'];
-            $price_format = $profile['data'][$i]['product']['price_format'];
-            $shop_name = $profile['data'][$i]['shop']['name'];
-            $shop_location = $profile['data'][$i]['shop']['location'];
-            
-            $data_arr['data'][] = array('image_url' => $image_url, 'product_name' => $product_name, 'price_format' => $price_format, 'shop_name' => $shop_name, 'shop_location' => $shop_location);
+            $id = $profile['data']['products'][$i]['id'];
+            $image_url = $profile['data']['products'][$i]['image_url'];
+            $product_name = $profile['data']['products'][$i]['name'];
+            $price_format = $profile['data']['products'][$i]['price'];
+            $price = $profile['data']['products'][$i]['price_int'];
+            $shop_name = $profile['data']['products'][$i]['shop']['name'];
+            $shop_location = $profile['data']['products'][$i]['shop']['location'];
+            $product_url = $profile['data']['products'][$i]['url'];
+
+            // ads item
+            // $id = $profile['data'][$i]['product']['id'];
+            // $image_url = $profile['data'][$i]['product']['image']['m_url'];
+            // $product_name = $profile['data'][$i]['product']['name'];
+            // $price_format = $profile['data'][$i]['product']['price_format'];
+            // $price = $profile['data'][$i]['product']['price'];
+            // $shop_name = $profile['data'][$i]['shop']['name'];
+            // $shop_location = $profile['data'][$i]['shop']['location'];
+
+            $search = "mouse";
+
+            $data_arr['data'][] = array('id' => $id, 'image_url' => $image_url, 'product_name' => $product_name, 'price_format' => $price_format, 'price' => $price, 'shop_name' => $shop_name, 'shop_location' => $shop_location, 'product_url' => $product_url, 'keyword' => $search);
             $i++;
         }
-        return View::make('home', array('data_arr'=>$data_arr));
+        return View::make('home', array('data_arr' => $data_arr, 'cart_data' => $cartCollection, 'data_city' => $data_city));
 
     }
     public function find()
     {
         if (isset($_POST['find'])) {
             $data_arr = array();
+
+            //list all available city in tokopedia
+            $city_url = "https://ace.tokopedia.com/v4/dynamic_attributes?scheme=https&device=desktop&related=true&page=9&ob=23&st=product&fcity=0&source=search&q=&unique_id=c8c1be17273f45f9b5d1c5346b116570&safe_search=false";
+            $data_city = http_request($city_url);
+            $data_city = json_decode($data_city, TRUE);
             
             //tokopedia
             $search = $_POST['text_value'];
+            $location = $_POST['location'];
         
             $search = str_replace(' ', '%20', $search);
         
-            $url = "https://ta.tokopedia.com/promo/v1/display/ads?user_id=0&ep=product&item=10&src=search&device=desktop&page=2&q=" . $search . "&fshop=1";
+            $url = "https://ace.tokopedia.com/search/product/v3?scheme=https&device=desktop&related=true&catalog_rows=5&source=search&ob=23&st=product&rows=200&start=0&fcity=".$location."&q=" .$search. "&unique_id=a5e21c08aa434ccda179065dc7e41c73&safe_search=false";
+            
+            // ads url
+            // $url = "https://ta.tokopedia.com/promo/v1/display/ads?user_id=0&ep=product&item=10&src=search&device=desktop&page=2&q=" . $search . "&fshop=1";
         
             $profile = http_request($url);
         
@@ -77,23 +128,196 @@ class HomeController extends Controller
             //     echo '<button type="submit" id="add" name="add">Add to Cart</button><br><br>';
             //     $i = $i + 1;
             // }
-        
+
             //new loop
             $i = 0;
             foreach ($profile["data"] as $profil) {
-                $image_url = $profile['data'][$i]['product']['image']['m_url'];
-                $product_name = $profile['data'][$i]['product']['name'];
-                $price_format = $profile['data'][$i]['product']['price_format'];
-                $shop_name = $profile['data'][$i]['shop']['name'];
-                $shop_location = $profile['data'][$i]['shop']['location'];
+                $id = $profile['data']['products'][$i]['id'];
+                $image_url = $profile['data']['products'][$i]['image_url'];
+                $product_name = $profile['data']['products'][$i]['name'];
+                $price_format = $profile['data']['products'][$i]['price'];
+                $price = $profile['data']['products'][$i]['price_int'];
+                $shop_name = $profile['data']['products'][$i]['shop']['name'];
+                $shop_location = $profile['data']['products'][$i]['shop']['location'];
+                $product_url = $profile['data']['products'][$i]['url'];
+
+                // ads item
+                // $id = $profile['data'][$i]['product']['id'];
+                // $image_url = $profile['data'][$i]['product']['image']['m_url'];
+                // $product_name = $profile['data'][$i]['product']['name'];
+                // $price_format = $profile['data'][$i]['product']['price_format'];
+                // $price = $profile['data'][$i]['product']['price'];
+                // $shop_name = $profile['data'][$i]['shop']['name'];
+                // $shop_location = $profile['data'][$i]['shop']['location'];
                 
-                $data_arr['data'][] = array('image_url' => $image_url, 'product_name' => $product_name, 'price_format' => $price_format, 'shop_name' => $shop_name, 'shop_location' => $shop_location);
+                $data_arr['data'][] = array('id' => $id, 'image_url' => $image_url, 'product_name' => $product_name, 'price_format' => $price_format, 'price' => $price, 'shop_name' => $shop_name, 'shop_location' => $shop_location, 'product_url' => $product_url, 'keyword' => $search);
                 $i++;
             }
-            return View::make('home', array('data_arr'=>$data_arr));
+            
+            $cartCollection = \Cart::getContent();
+
+            $cartCollection->toArray();
+
+            return View::make('home', array('data_arr'=>$data_arr, 'cart_data' => $cartCollection, 'data_city' => $data_city));
+            //dd($data_arr);
             // print "<pre>";
             // print_r($data_arr);
             // print "</pre>";
+        }
+        if (isset($_POST['add_button'])){
+
+            $data_arr = array();
+
+            //list all available city in tokopedia
+            $city_url = "https://ace.tokopedia.com/v4/dynamic_attributes?scheme=https&device=desktop&related=true&page=9&ob=23&st=product&fcity=0&source=search&q=&unique_id=c8c1be17273f45f9b5d1c5346b116570&safe_search=false";
+            $data_city = http_request($city_url);
+            $data_city = json_decode($data_city, TRUE);
+            
+            foreach($data_city['data']['filter'][0]['options'] as $city){
+                if (strpos($city['name'], $_POST['shop_location']) !== false){
+                    $city_code = $city['value'];
+                    break;
+                } 
+            }
+
+            //tokopedia
+            $search = $_POST['text_value'];
+            $location = $city_code;
+
+            $search = str_replace(' ', '%20', $search);
+        
+            $url = "https://ace.tokopedia.com/search/product/v3?scheme=https&device=desktop&related=true&catalog_rows=5&source=search&ob=23&st=product&rows=200&start=0&fcity=".$location."&q=" .$search. "&unique_id=a5e21c08aa434ccda179065dc7e41c73&safe_search=false";
+            
+            // ads url
+            // $url = "https://ta.tokopedia.com/promo/v1/display/ads?user_id=0&ep=product&item=10&src=search&device=desktop&page=2&q=" . $search . "&fshop=1";
+        
+            $profile = http_request($url);
+        
+            //replace JSON string to Array
+            $profile = json_decode($profile, TRUE);
+
+            $i = 0;
+            foreach ($profile["data"] as $profil) {
+
+                $id = $profile['data']['products'][$i]['id'];
+                $image_url = $profile['data']['products'][$i]['image_url'];
+                $product_name = $profile['data']['products'][$i]['name'];
+                $price_format = $profile['data']['products'][$i]['price'];
+                $price = $profile['data']['products'][$i]['price_int'];
+                $shop_name = $profile['data']['products'][$i]['shop']['name'];
+                $shop_location = $profile['data']['products'][$i]['shop']['location'];
+                $product_url = $profile['data']['products'][$i]['url'];
+
+                // ads item
+                // $id = $profile['data'][$i]['product']['id'];
+                // $image_url = $profile['data'][$i]['product']['image']['m_url'];
+                // $product_name = $profile['data'][$i]['product']['name'];
+                // $price_format = $profile['data'][$i]['product']['price_format'];
+                // $price = $profile['data'][$i]['product']['price'];
+                // $shop_name = $profile['data'][$i]['shop']['name'];
+                // $shop_location = $profile['data'][$i]['shop']['location'];
+                
+                $data_arr['data'][] = array('id' => $id, 'image_url' => $image_url, 'product_name' => $product_name, 'price_format' => $price_format, 'price' => $price, 'shop_name' => $shop_name, 'shop_location' => $shop_location, 'product_url' => $product_url, 'keyword' => $search);
+                $i++;
+            }
+
+            //cart
+            $id = $_POST['id'];
+            $image_url = $_POST['image_url'];
+            $product_name = $_POST['product_name'];
+            $price_format = $_POST['price_format'];
+            $price = $_POST['price'];
+            $shop_name = $_POST['shop_name'];
+            $shop_location = $_POST['shop_location'];
+
+            \Cart::add(array(
+                'id' => $id,
+                'name' => $product_name,
+                'price' => $price,
+                'quantity' => 1,
+                'attributes' => array(
+                    'image_url' => $image_url,
+                    'price_format' => $price_format,
+                    'shop_name' => $shop_name,
+                    'shop_location' => $shop_location,
+                    'keyword' => $search,
+                    'product_url' => $product_url
+                )
+            ));
+    
+            $cartCollection = \Cart::getContent();
+    
+            $cartCollection->toArray();
+
+            return View::make('home', array('cart_data' => $cartCollection, 'data_arr' => $data_arr, 'data_city' => $data_city));
+        }
+        if (isset($_POST['delete_button'])){
+
+            $data_arr = array();
+
+            //list all available city in tokopedia
+            $city_url = "https://ace.tokopedia.com/v4/dynamic_attributes?scheme=https&device=desktop&related=true&page=9&ob=23&st=product&fcity=0&source=search&q=&unique_id=c8c1be17273f45f9b5d1c5346b116570&safe_search=false";
+            $data_city = http_request($city_url);
+            $data_city = json_decode($data_city, TRUE);
+
+            foreach($data_city['data']['filter'][0]['options'] as $city){
+                if (strpos($city['name'], $_POST['shop_location']) !== false){
+                    $city_code = $city['value'];
+                    break;
+                } 
+            }
+            
+            //tokopedia
+            $search = $_POST['text_value'];
+            $location = $city_code;
+        
+            $search = str_replace(' ', '%20', $search);
+        
+            $url = "https://ace.tokopedia.com/search/product/v3?scheme=https&device=desktop&related=true&catalog_rows=5&source=search&ob=23&st=product&rows=200&start=0&fcity=".$location."&q=" .$search. "&unique_id=a5e21c08aa434ccda179065dc7e41c73&safe_search=false";
+            
+            // ads url
+            // $url = "https://ta.tokopedia.com/promo/v1/display/ads?user_id=0&ep=product&item=10&src=search&device=desktop&page=2&q=" . $search . "&fshop=1";
+        
+            $profile = http_request($url);
+        
+            //replace JSON string to Array
+            $profile = json_decode($profile, TRUE);
+
+            $i = 0;
+            foreach ($profile["data"] as $profil) {
+
+                $id = $profile['data']['products'][$i]['id'];
+                $image_url = $profile['data']['products'][$i]['image_url'];
+                $product_name = $profile['data']['products'][$i]['name'];
+                $price_format = $profile['data']['products'][$i]['price'];
+                $price = $profile['data']['products'][$i]['price_int'];
+                $shop_name = $profile['data']['products'][$i]['shop']['name'];
+                $shop_location = $profile['data']['products'][$i]['shop']['location'];
+                $product_url = $profile['data']['products'][$i]['url'];
+
+
+                // ads item
+                // $id = $profile['data'][$i]['product']['id'];
+                // $image_url = $profile['data'][$i]['product']['image']['m_url'];
+                // $product_name = $profile['data'][$i]['product']['name'];
+                // $price_format = $profile['data'][$i]['product']['price_format'];
+                // $price = $profile['data'][$i]['product']['price'];
+                // $shop_name = $profile['data'][$i]['shop']['name'];
+                // $shop_location = $profile['data'][$i]['shop']['location'];
+                
+                $data_arr['data'][] = array('id' => $id, 'image_url' => $image_url, 'product_name' => $product_name, 'price_format' => $price_format, 'price' => $price, 'shop_name' => $shop_name, 'shop_location' => $shop_location, 'product_url' => $product_url, 'keyword' => $search);
+                $i++;
+            }
+
+            $delete_id = $_POST['id_delete'];
+
+            \Cart::remove($delete_id);
+
+            $cartCollection = \Cart::getContent();
+            $cartCollection->toArray();
+
+            return View::make('home', array('data_arr' => $data_arr, 'cart_data' => $cartCollection, 'data_city' => $data_city));
+
         }
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,32 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-
 <div class="container justify-content-center content">
-    <!-- filter -->
-    <h5 class="filter-title">Filter</h5>
-    <form class="filter" method="POST">
-        <label class="label-filter">Location</label>
-        <div class="form-check form-check-inline">
-
-            <select class="filter-kota">
-                <option value=""></option>
-                <option value=""></option>
-                <option value=""></option>
-                <option value=""></option>
-            </select>
-
-        </div>
-        <div class="form-check form-check-inline">
-            <input class="form-check-input" type="text" id="harga-maks">
-            <label class="form-check-label" for="harga-maks">Harga Maksimal</label>
-        </div>
-        <div class="form-check form-check-inline">
-            <input class="form-check-input" type="text" id="harga-min">
-            <label class="form-check-label" for="harga-min">Harga Minimal</label>
-        </div>
-    </form>
-
 
     <!-- Tabs -->
     <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
@@ -39,12 +14,33 @@
     </ul>
 
     <!-- Search -->
-    <form action="/home" method="POST">
+    <form action="/home" method="POST" id="filter_form">
         @csrf
         <div class="tab-content" id="pills-tabContent">
             <div class="tab-pane fade show active" id="pills-name" role="tabpanel" aria-labelledby="pills-name-tab">
+                <!-- filter -->
+                <h5 class="filter-title">Filter</h5>
+
+                <label class="label-filter">Location</label>
+                <div class="form-check form-check-inline">
+                    <select class="filter-kota" name="location">
+                        <option value="">Semua</option>
+                        @foreach($data_city['data']['filter'][0]['options'] as $city)
+                        <option value="{{ $city['value'] }}">{{ $city['name'] }}</option>
+                        @endforeach
+                    </select>
+
+                </div>
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="text" id="harga-maks">
+                    <label class="form-check-label" for="harga-maks">Harga Maksimal</label>
+                </div>
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="text" id="harga-min">
+                    <label class="form-check-label" for="harga-min">Harga Minimal</label>
+                </div>
                 <div class="input-group search">
-                    <input type="text" name="text_value" class="form-control input-search" placeholder="Nama Barang">
+                    <input type="text" name="text_value" class="form-control input-search" placeholder="Nama Barang" value="{{ str_replace('%20', ' ', $data_arr['data'][0]['keyword']) }}">
                     <button class="btn btn-default" name="find" type="submit"><img class="icon-search" src="./svg/magnifying-glass.svg"></button>
                 </div>
             </div>
@@ -59,20 +55,36 @@
 
     <!-- List-Items-Added -->
     <div class="item-container-add">
-        <div class="row row-add">
-            <div class="col-sm-4">
-                <img class="item-add-img" src="./svg/person.svg">
+
+        @foreach($cart_data as $cart)
+        @if($cart['name'] != 'asdfghjklkjgfds123890ythbnvdkodetokopediafwgheu3yr2t3r64ortfg')
+        <form action="/home" method="POST">
+            @csrf
+            <div class="row row-add">
+                <div class="col-sm-4">
+                    <img class="item-add-img" src="{{ $cart['attributes']['image_url'] }}">
+                </div>
+                <div class="col-sm-7">
+                    <input type="hidden" name="id_delete" value="{{ $cart['id'] }}">
+
+                    <p class="text-namabarang-add">{{ $cart['name'] }}</p>
+                    <p class="text-harga-add">{{ $cart['attributes']['price_format'] }}</p>
+                    <p class="text-namatoko-add">{{ $cart['attributes']['shop_name'] }}</p>
+
+                    <p class="text-lokasitoko-add">{{ $cart['attributes']['shop_location'] }}</p>
+                    <input type="hidden" name="shop_location" value="{{ $cart['attributes']['shop_location'] }}">
+
+                    <input type="hidden" name="text_value" value="{{ $cart['attributes']['keyword'] }}">
+                </div>
+                <div class="col-sm-1">
+                    <!-- <a class="item-deletebtn" ><img class="icon-delete" src="./svg/delete.svg"></a> -->
+                    <button name="delete_button" type="submit">Delete</button>
+                </div>
             </div>
-            <div class="col-sm-7">
-                <p class="text-namabarang-add">Nama Barang</p>
-                <p class="text-harga-add">50000000000</p>
-                <p class="text-namatoko-add">Nama Toko</p>
-                <p class="text-lokasitoko-add">Lokasi Toko</p>
-            </div>
-            <div class="col-sm-1">
-                <a class="item-deletebtn" href="#"><img class="icon-delete" src="./svg/delete.svg"></a>
-            </div>
-        </div>
+        </form>
+        @endif
+        @endforeach
+
     </div>
 
     <!-- List-Items-Searched-Tokopedia -->
@@ -82,28 +94,45 @@
 
             <!-- Cards -->
             <div class="carousel-inner row mx-auto">
-           
-            
-            <!-- coba for print dalamnya php-->
-            @for ($i = 0; $i < 10; $i++)
-                @if ($i == 0)
-                    <div class="carousel-item col-md-3 active">
-                @elseif ($i > 0)
+
+                <!-- coba for print dalamnya php-->
+                @for ($i = 0; $i < 10; $i++) @if ($i==0) <div class="carousel-item col-md-3 active">
+                    @elseif ($i > 0)
                     <div class="carousel-item col-md-3">
-                @endif
-                    <a href="#">
-                        <div class="card item">
-                            <img class="card-img-top img-fluid" src="{{$data_arr['data'][$i]['image_url']}}">
-                            <div class="card-body">
-                                <h4 class="card-title">{{$data_arr['data'][$i]['product_name']}}</h4>
-                                <p class="card-text">{{$data_arr['data'][$i]['price_format']}}</p>
-                                <p class="card-text">{{$data_arr['data'][$i]['shop_name']}}</p>
-                                <p class="card-text">{{$data_arr['data'][$i]['shop_location']}}</p>
+                        @endif
+                        <a href="{{ $data_arr['data'][$i]['product_url'] }}">
+                            <div class="card item">
+                                <form action="/home" method="POST" id="list_tokopedia_form">
+                                    @csrf
+                                    <img class="card-img-top img-fluid" src="{{$data_arr['data'][$i]['image_url']}}">
+                                    <input type="hidden" name="image_url" value="{{$data_arr['data'][$i]['image_url']}}">
+                                    <div class="card-body">
+                                        <input type="hidden" name="id" value="{{$data_arr['data'][$i]['id']}}">
+
+                                        <h4 class="card-title">{{$data_arr['data'][$i]['product_name']}}</h4>
+                                        <input type="hidden" name="product_name" value="{{$data_arr['data'][$i]['product_name']}}">
+
+                                        <p class="card-text">{{$data_arr['data'][$i]['price_format']}}</p>
+                                        <input type="hidden" name="price_format" value="{{$data_arr['data'][$i]['price_format']}}">
+
+                                        <p class="card-text" hidden>{{$data_arr['data'][$i]['price']}}</p>
+                                        <input type="hidden" name="price" value="{{$data_arr['data'][$i]['price']}}">
+
+                                        <p class="card-text">{{$data_arr['data'][$i]['shop_name']}}</p>
+                                        <input type="hidden" name="shop_name" value="{{$data_arr['data'][$i]['shop_name']}}">
+
+                                        <p class="card-text">{{$data_arr['data'][$i]['shop_location']}}</p>
+                                        <input type="hidden" name="shop_location" value="{{$data_arr['data'][$i]['shop_location']}}">
+
+                                        <input type="hidden" name="text_value" value="{{ $data_arr['data'][$i]['keyword'] }}">
+
+                                        <button name="add_button" type="submit" onclick="submitForms()">Add</button>
+                                    </div>
+                                </form>
                             </div>
-                        </div>
-                    </a>
-                </div>
-            @endfor
+                        </a>
+                    </div>
+                    @endfor
             </div>
             <a class="carousel-control-prev" href="#Item-list-Tokopedia" role="button" data-slide="prev">
                 <img class="icon-prev" src="./svg/chevron-left.svg">
@@ -226,4 +255,9 @@
         </div>
     </div>
 </div>
+<script>
+    submitForms = function(){
+        document.getElementById('filter_form')
+    }
+</script>
 @endsection
