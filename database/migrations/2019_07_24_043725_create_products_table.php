dd6791b4b084e07767e9700ced3c934683efcc5b
Nicolas <m26416088@john.petra.ac.id>
2019-07-24 13:15:20 +0800

    table done

---
+++ database/migrations/2019_07_24_043725_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('product_name');
+            $table->string('image_url');
+            $table->string('shop_name');
+            $table->string('shop_location');
+            $table->string('product_url');
+            $table->integer('id_search')->references('id')->on('searches');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
